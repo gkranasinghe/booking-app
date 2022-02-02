b0103bb6e09921a37b4d8065585e05a80eee62b6
gkranasinghe <gkranasinghe@gmail.com>
2022-02-02 10:33:38 +0530

    printf

--- main.go
+++ main.go
@@ -5,12 +5,24 @@ import "fmt"
 func main(){
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50 
-	var remainingTickets = 50 
+	var remainingTickets uint = 50 
+    
+    fmt.Printf("conferenceTickets is %T\n",conferenceTickets)
+	fmt.Printf("remainingTickets is %T\n",remainingTickets)
 
-	fmt.Println("Welcome to ",conferenceName," go conference ")
-	fmt.Println("We have a total of  ",conferenceTickets," tickets and ",remainingTickets," remainingTickets availlable ")
+	fmt.Printf("Welcome to %v  \n",conferenceName)
+	fmt.Printf("We have a total of  %v tickets and %v remainingTickets availlable " ,conferenceTickets,remainingTickets)
 	fmt.Println("Get tickets to atttend here  ")
+     
 
+	var userName string
+	var userTickets int 
+
+	userName = "Tom"
+	userTickets = 2
+
+
+	fmt.Printf("User %v booked %v tickets.\n" ,userName,userTickets)
 
 
 }
