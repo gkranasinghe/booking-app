c4e7eaab2091e655bb8c591d345b11872746015f
gkranasinghe <gkranasinghe@gmail.com>
2022-02-02 10:53:28 +0530

    scan

--- main.go
+++ main.go
@@ -10,19 +10,28 @@ func main(){
     fmt.Printf("conferenceTickets is %T\n",conferenceTickets)
 	fmt.Printf("remainingTickets is %T\n",remainingTickets)
 
-	fmt.Printf("Welcome to %v  \n",conferenceName)
+	fmt.Printf("Welcome to %v  \n",&conferenceName)
 	fmt.Printf("We have a total of  %v tickets and %v remainingTickets availlable " ,conferenceTickets,remainingTickets)
 	fmt.Println("Get tickets to atttend here  ")
      
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int 
 
-	userName = "Tom"
-	userTickets = 2
 
+	fmt.Println("Enter first name:")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v booked %v tickets.\n" ,userName,userTickets)
+	fmt.Println("Enter last name:")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter email:")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("User %v %v  booked %v tickets\nyou will recieve confirmation at %v \n" ,firstName,lastName,userTickets,email)
 
 
 }
